a35c26f20eb567be0f3edb66fde065edfa44b844
mariyasada <mariyasada2406@gmail.com>
2022-03-25 13:59:59 +0530

    feat:add homepage to notes app

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Footer } from "../../Component";
+import "./Home.css";
+
+export const HomePage = () => {
+  return (
+    <div className="HomePage-container flex-center flex-direction-column ">
+      <div className="heading-of-notes">
+        <h1 className="heading">
+          <span className="name-of-app">MS</span> Notes
+        </h1>
+      </div>
+      <div className="image-quote-container flex-center">
+        <div className="image-container">
+          <img
+            className="image-of-homepage"
+            src="./assets/notes.png"
+            alt="notes image"
+          />
+        </div>
+        <div className="quote-of-app">
+          <p className="quote">
+            Organized your day by saving your thoughts with us.
+          </p>
+          <span className="btn-container flex-center">
+            <button className="btn btn-primary-first border-round">
+              TRY MS NOTES
+            </button>
+          </span>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
